0f425f49cc20e3b5fe84bacb4bb26eec6e141617
ThomasSeisveld <tseisveld@gmail.com>
2025-12-12 10:27:07 +0100

    tweaks

--- data/script.js
+++ data/script.js
@@ -1,5 +1,35 @@
-const gallery = document.querySelector('.gallery');
 const gridToggles = document.querySelectorAll('.grid-list-toggle');
+
+const extralargeBtn = document.querySelector('.grid-list-toggle-extralarge');
+const largeBtn = document.querySelector('.grid-list-toggle-large');
+const mediumBtn = document.querySelector('.grid-list-toggle-medium');
+const smallBtn = document.querySelector('.grid-list-toggle-small');
+const gallery = document.querySelector('.gallery');
+
+extralargeBtn.addEventListener("click", toggleextralarge);
+largeBtn.addEventListener("click", togglelarge);
+mediumBtn.addEventListener("click", togglemedium);
+smallBtn.addEventListener("click", togglesmall);
+
+function toggleextralarge() {
+  gallery.classList.remove('gallery-large', 'gallery-medium', 'gallery-small');
+  gallery.classList.add('gallery-extralarge');
+}
+
+function togglelarge() {
+  gallery.classList.remove('gallery-extralarge', 'gallery-medium', 'gallery-small');
+  gallery.classList.add('gallery-large');
+}
+
+function togglemedium() {
+  gallery.classList.remove('gallery-extralarge', 'gallery-large', 'gallery-small');
+  gallery.classList.add('gallery-medium');
+}
+
+function togglesmall() {
+  gallery.classList.remove('gallery-extralarge', 'gallery-large', 'gallery-medium');
+  gallery.classList.add('gallery-small');
+}
 
 // grid sizes 
 // const gridSizes = {
@@ -9,32 +39,32 @@ const gridToggles = document.querySelectorAll('.grid-list-toggle');
 //   'small': '12vw'
 // };
 
-gridToggles.forEach((button, index) => {
-  button.addEventListener('click', function(e) {
-    // e.preventDefault();
-    // const parent = this.closest('li');
-    let gridClass = '';
+// gridToggles.forEach((button, index) => {
+//   button.addEventListener('click', function(e) {
+//     let gridClass = '';
+
     
-    if (parent.classList.contains('large-grid')) {
-      gridClass = 'gallery-large';
-    } else if (parent.classList.contains('medium-grid')) {
-      gridClass = 'gallery-medium';
-    } else if (parent.classList.contains('small-grid')) {
-      gridClass = 'gallery-small';
-    }
     
-    gallery.classList.remove('gallery-large', 'gallery-medium', 'gallery-small');
+//     if (parent.classList.contains('large-grid')) {
+//       gridClass = 'gallery-large';
+//     } else if (parent.classList.contains('medium-grid')) {
+//       gridClass = 'gallery-medium';
+//     } else if (parent.classList.contains('small-grid')) {
+//       gridClass = 'gallery-small';
+//     }
     
-    if (gridClass) {
-      gallery.classList.add(gridClass);
-    }
+//     gallery.classList.remove('gallery-large', 'gallery-medium', 'gallery-small');
     
-    const details = document.querySelector('.grid-view-header details');
-    if (details) {
-      details.open = false;
-    }
-  });
-});
+//     if (gridClass) {
+//       gallery.classList.add(gridClass);
+//     }
+    
+//     const details = document.querySelector('.grid-view-header details');
+//     if (details) {
+//       details.open = false;
+//     }
+//   });
+// });
 
 // const captureBtn = document.querySelector('.capture');
 // const timeDisplay = document.querySelector('.capture-menu h2');
